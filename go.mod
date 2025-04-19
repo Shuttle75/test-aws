1defcf6c3bb079ac207aa1199887d0d5b9b79187
OleksiiOsadchyi <v-Oleksii.Osadchyi@syniverse.com>
2025-04-19 17:23:35 +0300

    Fix

--- go.mod
+++ go.mod
@@ -1,10 +1,35 @@
 module test-aws
 
-go 1.22
+go 1.23.0
 
-require github.com/redis/go-redis/v9 v9.7.3
+toolchain go1.24.0
 
 require (
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/aws/aws-dax-go-v2 v1.0.0
+	github.com/aws/aws-sdk-go-v2 v1.36.3
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.18.12
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.42.4
+	github.com/redis/go-redis/v9 v9.7.3
+)
+
+require (
+	github.com/antlr4-go/antlr/v4 v4.13.1 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.29.14 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.67 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.30 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.34 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.34 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.25.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.10.15 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.15 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.25.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.30.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.33.19 // indirect
+	github.com/aws/smithy-go v1.22.3 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/gofrs/uuid v4.4.0+incompatible // indirect
+	golang.org/x/exp v0.0.0-20250408133849-7e4ce0ab07d0 // indirect
 )
--- main.go
+++ main.go
@@ -2,18 +2,51 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"github.com/aws/aws-dax-go-v2/dax"
+	"github.com/aws/aws-dax-go-v2/dax/utils"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/redis/go-redis/v9"
 	"log"
 )
 
 //aws ssm start-session --target i-0a283e67790f4c8c7 --document-name AWS-StartPortForwardingSessionToRemoteHost --parameters '{\"host\":[\"redis-4guxsh.serverless.euw2.cache.amazonaws.com\"],\"portNumber\":[\"6379\"],\"localPortNumber\":[\"6379\"]}'
 
-// var limiterRedis = throttling.NewLimiter(5)
 var client = redis.NewClient(&redis.Options{
 	Addr:                  "redis-4guxsh.serverless.euw2.cache.amazonaws.com:6379",
 	ContextTimeoutEnabled: false,
 	DB:                    0,
 })
+
+var endpoint = "daxs://dax-cluster.4guxsh.dax-clusters.eu-west-2.amazonaws.com"
+var daxClient, _ = daxClientNew(endpoint, "eu-west-2")
+
+func daxClientNew(endpoint, region string) (*dax.Dax, error) {
+	cfg := dax.DefaultConfig()
+	cfg.HostPorts = []string{endpoint}
+	cfg.Region = region
+	cfg.SkipHostnameVerification = false
+	//cfg.ClientHealthCheckInterval = 30 * time.Second
+	//cfg.IdleConnectionReapDelay = 30 * time.Second
+	//cfg.MaxPendingConnectionsPerHost = 100
+	cfg.LogLevel = utils.LogDebug
+	//cfg.DialContext = func(ctx context.Context, network string, address string) (net.Conn, error) {
+	//	// fmt.Println("Write your custom logic here")
+	//	dialCon, err := dax.SecureDialContext(endpoint, cfg.SkipHostnameVerification)
+	//	if err != nil {
+	//		panic(fmt.Errorf("secure dialcontext creation failed %v", err))
+	//	}
+	//	return dialCon(ctx, network, address)
+	//}
+	client, err := dax.New(cfg)
+	if err != nil {
+		panic(fmt.Errorf("unable to initialize client %v", err))
+	}
+	return client, err
+}
 
 func main() {
 	setErr := client.Set(context.Background(), "1234", "5678", 0).Err()
@@ -25,5 +58,25 @@ func main() {
 		log.Printf("Couldn't get item: %v\n", getErr)
 	} else {
 		log.Printf("Get item: %v\n", value)
+	}
+
+	in := dynamodb.GetItemInput{
+		Key: map[string]types.AttributeValue{
+			"DestOperatorId": &types.AttributeValueMemberS{Value: "0123456789"},
+		},
+		TableName: aws.String("thr_req_to_dest_operator"),
+	}
+	var item struct {
+		DestOperatorId string
+		LastReq        int64
+	}
+	out, err := daxClient.GetItem(context.Background(), &in)
+	if err != nil {
+		log.Printf("Couldn't get info about %v. Here's why: %v\n", "0123456789", err)
+	} else {
+		errMap := attributevalue.UnmarshalMap(out.Item, &item)
+		if errMap != nil {
+			log.Printf("Couldn't unmarshal response. Here's why: %v\n", err)
+		}
 	}
 }
